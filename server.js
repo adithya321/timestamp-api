cad4d6354cbcbe8b5e98a6f2769efcf20809aee6
Adithya J <adithya321@hotmail.com>
2016-07-31 12:14:15 +0530

    Unix to Natural

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 var express = require('express');
+var moment = require('moment');
 var app = express();
 var port = process.env.PORT || 8080;
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+	res.sendFile(process.cwd() + '/public/index.html');
+});
+
+app.get('/:query', function(req, res) {
+	var unix = req.params.query;
+	var natural = moment.unix(unix).format("MMMM D, YYYY");
+
+	var dateObj = { "unix": unix, "natural": natural };
+	res.send(dateObj);
 });
 
 app.listen(port, function () {
-  console.log('Example app listening on port 8080!');
+	console.log('Example app listening on port ' + port);
 });
